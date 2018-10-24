ffadb46fc30c8a0b9cec03504689142dcf00b33b
Varun Arora <v.arora.mail@gmail.com>
2018-10-24 18:08:06 +0000

    some comments, test commit

--- keras-perceptron/perceptron-single.py
+++ keras-perceptron/perceptron-single.py
@@ -1,6 +1,6 @@
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Flatten, Dropout
+from keras.datasets import mnist #data
+from keras.models import Sequential #each layer feeds into next layer
+from keras.layers import Dense, Flatten, Dropout #NN is a series of layers, we grabbed some
 from keras.utils import np_utils
 
 import wandb
@@ -13,6 +13,7 @@ config = run.config
 # load data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
+#Convert our training sets into an array of 1 for 5 and 0 for not 5, uses numpy
 is_five_train = y_train == 5
 is_five_test = y_test == 5
 labels = ["Not Five", "Is Five"]
@@ -22,8 +23,10 @@ img_height = X_train.shape[2]
 
 # create model
 model=Sequential()
-model.add(Flatten(input_shape=(img_width,img_height)))
-model.add(Dense(1))
+model.add(Flatten(input_shape=(img_width,img_height))) #2D to 1D array
+model.add(Dense(1)) #We have 1 perceptron, densely connected. Every input from old layer goes into 1 perceptron (how many outputs)
+#how do we measure how far we stray from the truth, mse = mean squared error.
+#Need to minimize loss
 model.compile(loss='mse', optimizer='adam',
                 metrics=['accuracy'])
 
